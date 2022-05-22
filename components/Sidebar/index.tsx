e6796ec26cbf243433ab387212db94a03f15b94f
millanadhikari <millanadhikari@gmail.com>
2022-05-22 11:23:24 +1000

    sidebar with header skeleton done

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -18,23 +18,28 @@ import {
 } from "@chakra-ui/react";
 import { signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
-import { MdOutlineSettings, MdOutlineSettingsAccessibility, MdOutlineSettingsSuggest } from "react-icons/md";
+import { AiOutlineLogout } from "react-icons/ai";
 import {
-    RiBilibiliLine,
-    RiBilliardsLine,
-    RiBillLine,
+  MdOutlineSettings,
+  MdOutlineSettingsAccessibility,
+  MdOutlineSettingsSuggest,
+} from "react-icons/md";
+import {
+  RiBilibiliLine,
+  RiBilliardsLine,
+  RiBillLine,
   RiHomeSmile2Line,
   RiMailSendLine,
   RiMoneyDollarCircleLine,
 } from "react-icons/ri";
 
 interface Props {
-  onClose: Function;
+  onClose: React.MouseEventHandler<HTMLDivElement> 
   isOpen: boolean;
   variant: "drawer" | "sidebar";
 }
 
-const SidebarContent = ({ onClick }: { onClick: Function }) => (
+const SidebarContent = ({ onClick }: { onClick: React.MouseEventHandler<HTMLDivElement> | undefined }) => (
   <Flex direction="column" textAlign="left" ml={-5}>
     <Text textAlign="left" fontWeight="semibold" mb={2} ml={5}>
       Menu
@@ -47,155 +52,176 @@ const SidebarContent = ({ onClick }: { onClick: Function }) => (
       _focus={{ outline: "none", border: "none" }}
     >
       <TabList>
-       <Flex flexDirection="column" gap={3}>
-       <Tab
-          _focus={{ outline: "none" }}
-          _selected={{
-            color: "red",
-            borderLeft: "4px solid blue",
-            borderColor: "red.500",
-          }}
-        >
-          <Flex
-            alignItems="center"
-            gap={3}
-            onClick={() => onClick}
-            bg="none"
-            fontWeight="semibold"
+        <Flex flexDirection="column" gap={3}>
+          <Tab
+            _focus={{ outline: "none" }}
+            _selected={{
+              color: "red",
+              borderLeft: "4px solid blue",
+              borderColor: "red.500",
+            }}
           >
-            <Box fontSize="18px">
-              {" "}
-              <RiHomeSmile2Line />{" "}
-            </Box>
-            <Text>Home</Text>
-          </Flex>
-        </Tab>
-        <Tab
-          _focus={{ outline: "none" }}
-          _selected={{
-            color: "red",
-            borderLeft: "4px solid blue",
-            borderColor: "red.500",
-          }}
-        >
-          <Flex
-            alignItems="center"
-            gap={3.5}
-            onClick={() => onClick}
-            bg="none"
-            fontWeight="semibold"
+            <Flex
+              alignItems="center"
+              gap={3}
+              onClick={onClick}
+              bg="none"
+              fontWeight="semibold"
+            >
+              <Box fontSize="18px">
+                {" "}
+                <RiHomeSmile2Line />{" "}
+              </Box>
+              <Text>Home</Text>
+            </Flex>
+          </Tab>
+          <Tab
+            _focus={{ outline: "none" }}
+            _selected={{
+              color: "red",
+              borderLeft: "4px solid blue",
+              borderColor: "red.500",
+            }}
           >
-            <Box fontSize="18px">
-              {" "}
-              <RiMailSendLine />{" "}
-            </Box>
-            <Text>Inbox</Text>
-          </Flex>
-        </Tab>
-        <Tab
-          _focus={{ outline: "none" }}
-          _selected={{
-            color: "red",
-            borderLeft: "4px solid blue",
-            borderColor: "red.500",
-          }}
-        >
-          <Flex
-            alignItems="center"
-            gap={4}
-            pl="3"
-            onClick={() => onClick}
-            bg="none"
-            fontWeight="semibold"
+            <Flex
+              alignItems="center"
+              gap={3.5}
+              onClick={() => onClick}
+              bg="none"
+              fontWeight="semibold"
+            >
+              <Box fontSize="18px">
+                {" "}
+                <RiMailSendLine />{" "}
+              </Box>
+              <Text>Inbox</Text>
+            </Flex>
+          </Tab>
+          <Tab
+            _focus={{ outline: "none" }}
+            _selected={{
+              color: "red",
+              borderLeft: "4px solid blue",
+              borderColor: "red.500",
+            }}
           >
-            <Box fontSize="18px">
-              {" "}
-              <RiMoneyDollarCircleLine />{" "}
-            </Box>
-            <Text>Quotes</Text>
-          </Flex>
-        </Tab>
-        <Tab
-          _focus={{ outline: "none" }}
-          _selected={{
-            color: "red",
-            borderLeft: "4px solid blue",
-            borderColor: "red.500",
-          }}
-        >
-          <Flex
-            alignItems="center"
-            gap={4}
-            pl="4"
-            onClick={() => onClick}
-            bg="none"
-            fontWeight="semibold"
+            <Flex
+              alignItems="center"
+              gap={4}
+              pl="3"
+              onClick={() => onClick}
+              bg="none"
+              fontWeight="semibold"
+            >
+              <Box fontSize="18px">
+                {" "}
+                <RiMoneyDollarCircleLine />{" "}
+              </Box>
+              <Text>Quotes</Text>
+            </Flex>
+          </Tab>
+          <Tab
+            _focus={{ outline: "none" }}
+            _selected={{
+              color: "red",
+              borderLeft: "4px solid blue",
+              borderColor: "red.500",
+            }}
           >
-            <Box fontSize="18px">
-              {" "}
-              <RiBilibiliLine />
-            </Box>
-            <Text>Invoices</Text>
-          </Flex>
-        </Tab>
-        <Tab
-          _focus={{ outline: "none" }}
-          _selected={{
-            color: "red",
-            borderLeft: "4px solid blue",
-            borderColor: "red.500",
-          }}
-        >
-          <Flex
-            alignItems="center"
-            gap={4}
-            pl="4"
-            onClick={() => onClick}
-            bg="none"
-            fontWeight="semibold"
+            <Flex
+              alignItems="center"
+              gap={4}
+              pl="4"
+              onClick={() => onClick}
+              bg="none"
+              fontWeight="semibold"
+            >
+              <Box fontSize="18px">
+                {" "}
+                <RiBilibiliLine />
+              </Box>
+              <Text>Invoices</Text>
+            </Flex>
+          </Tab>
+          <Tab
+            _focus={{ outline: "none" }}
+            _selected={{
+              color: "red",
+              borderLeft: "4px solid blue",
+              borderColor: "red.500",
+            }}
           >
-            <Box fontSize="18px">
-              {" "}
-              <MdOutlineSettingsSuggest />
-            </Box>
-            <Text>Settings</Text>
-          </Flex>
-        </Tab>
-        
-       </Flex>
+            <Flex
+              alignItems="center"
+              gap={4}
+              pl="4"
+              onClick={() => onClick}
+              bg="none"
+              fontWeight="semibold"
+            >
+              <Box fontSize="18px">
+                {" "}
+                <MdOutlineSettingsSuggest />
+              </Box>
+              <Text>Settings</Text>
+            </Flex>
+          </Tab>
+          <Text
+            textAlign="left"
+            color="gray.600"
+            mt={5}
+            fontWeight="semibold"
+            mb={2}
+            ml={5}
+          >
+            Favorites
+          </Text>
+        </Flex>
       </TabList>
     </Tabs>
-    
-    <Box position="absolute" bottom="0" onClick={() => signOut()} w="70%" mx="auto" >
+
+    <Flex
+      position="absolute"
+      bottom="1"
+      onClick={() => signOut()}
+      rounded="md"
+      cursor="pointer"
+      mx="auto"
+      alignItems="center"
+      gap={2}
+      bg="red.500"
+      px={4}
+      py={2}
+      color="gray.100"
+      textAlign="center"
+    >
+      <AiOutlineLogout />
       Logout
-    </Box>
+    </Flex>
   </Flex>
 );
 
 const Profile = () => {
-    return (
-        <Flex alignItems="center" gap={6} mx={6}>
-            <Image 
-                src="https://english.onlinekhabar.com/wp-content/uploads/2022/05/Balendra-Shaha-Balen-Sketch-1024x624-1.jpg"
-                h={12}
-                w={12}
-                rounded="full"
-
-                />
-            <Box my={3}>
-                <Heading fontSize="18px">AR Shakir</Heading>
-                <Text fontWeight="semibold" fontSize="13px" mt={1}>Sr. Admin Manager</Text>
-            </Box>
-
-
-
-        </Flex>
-
-    )
-}
+  return (
+    <Flex alignItems="center" gap={6} mx={6}>
+      <Image
+        src="https://english.onlinekhabar.com/wp-content/uploads/2022/05/Balendra-Shaha-Balen-Sketch-1024x624-1.jpg"
+        h={12}
+        w={12}
+        rounded="full"
+      />
+      <Box my={3}>
+        <Heading fontSize="18px">Balendra Shahi</Heading>
+        <Text fontWeight="semibold" fontSize="13px" mt={1}>
+          Sr. Admin Manager
+        </Text>
+      </Box>
+    </Flex>
+  );
+};
 const Sidebar = ({ isOpen, variant, onClose }: Props) => {
-    const { data: session } = useSession();
-//   const session = true;
+  const { data: session } = useSession();
+  //   const session = true;
   if (session) {
     return variant === "sidebar" ? (
       <Box
@@ -210,12 +236,12 @@ const Sidebar = ({ isOpen, variant, onClose }: Props) => {
         <SidebarContent onClick={onClose} />
       </Box>
     ) : (
-      <Drawer isOpen={true} placement="left" onClose={() => onClose}>
+      <Drawer isOpen={isOpen} placement="left" onClose={ ()=>onClose}>
         <DrawerOverlay>
           <DrawerContent p={-8}>
             <DrawerCloseButton display={{ base: "none", md: "inline" }} />
             <DrawerHeader>Elite-ADMIN 1.0</DrawerHeader>
-            <Profile/>
+            <Profile />
             <DrawerBody>
               <SidebarContent onClick={onClose} />
             </DrawerBody>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,34 @@
 import "../styles/globals.css";
-import { ChakraProvider, useBreakpointValue } from "@chakra-ui/react";
+import { Box, ChakraProvider, useBreakpointValue } from "@chakra-ui/react";
 import { SessionProvider, useSession } from "next-auth/react";
 import { AppProps } from "next/app";
 import Sidebar from "../components/Sidebar";
 import { useState } from "react";
+import Header from "../components/Header";
 
 const smVariant = { navigation: "drawer", navigationButton: true };
 const mdVariant = { navigation: "sidebar", navigationButton: false };
 
-
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+  const variants = useBreakpointValue({ base: smVariant, md: mdVariant });
 
-     const [isSidebarOpen, setSidebarOpen] = useState(false);
-     const variants = useBreakpointValue({ base: smVariant, md: mdVariant });
-   
-     const toggleSidebar = () => setSidebarOpen(!isSidebarOpen);
-    
- 
+  const toggleSidebar = () => setSidebarOpen(!isSidebarOpen);
+
   return (
     <SessionProvider session={pageProps.session}>
       <ChakraProvider>
-      <Sidebar
+        <Sidebar
           variant={"drawer"}
           isOpen={isSidebarOpen}
-          onClose={toggleSidebar}/>
+          onClose={toggleSidebar}
+        />
+        <Box ml={!variants?.navigationButton ? 200 : 0}>
+          <Header
+            showSidebarButton={variants?.navigationButton}
+            onShowSidebar={toggleSidebar}
+          />
+        </Box>
         <Component {...pageProps} />
       </ChakraProvider>
     </SessionProvider>
